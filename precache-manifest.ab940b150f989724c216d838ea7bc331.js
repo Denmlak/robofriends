07a7da5b51b4e64c3bf1b5871f8ee20d389928da
Denmlak <55392010+Denmlak@users.noreply.github.com>
2019-11-05 18:18:44 +0100

    Updates

---
+++ precache-manifest.ab940b150f989724c216d838ea7bc331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689458988c022fc322ae16de86613bb3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2d8ef264539cd05130cb",
+    "url": "/robofriends/static/css/main.42700761.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "2d8ef264539cd05130cb",
+    "url": "/robofriends/static/js/main.d9792b43.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
